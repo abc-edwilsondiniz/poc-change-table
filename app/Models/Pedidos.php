1fd5fefc52f0297a9dd9aaf2c3cde7c57f15e6f4
joao.raminelli <joao.raminelli@abcdaconstrucao.com.br>
2023-04-27 13:53:45 -0300

    alterando para plural o nome das tabelas

--- app/Models/Pedidos.php
+++ app/Models/Pedidos.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class PediClicad extends Model {
+class Pedidos extends Model {
 
     use HasFactory;
 
@@ -14,6 +14,6 @@ class PediClicad extends Model {
      *
      * @var string
      */
-    protected $table = 'pediclicad';
+    protected $table = 'pedidos';
 
 }
